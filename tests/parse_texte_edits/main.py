4b5f02303749bd40d10425cb9d2f15a389b542f5
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2016-05-06 17:27:16 +0200

    Add tests for the parse_texte_edits.py script with some passing test cases.

---
+++ tests/parse_texte_edits/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import glob
+import os
+import subprocess
+import codecs
+
+# http://eli.thegreenplace.net/2014/04/02/dynamically-generating-python-test-cases
+class TestTexteEditsParser(unittest.TestCase):
+    pass
+
+def get_compare_outputs_fn(description, input_filename, output_filename):
+    def test(self):
+        process = subprocess.Popen(
+            'python ../../scripts/collectdata/parse_texte_edits.py ' + input_filename,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        out, err = process.communicate()
+        out = out.decode('utf-8')
+
+        output_data = codecs.open(output_filename, 'r', 'utf-8').read()
+        self.assertEqual(out, output_data, description)
+    return test
+
+if __name__ == '__main__':
+    inputs = glob.glob('input/*.json')
+
+    for input_filename in inputs:
+        description = os.path.splitext(os.path.basename(input_filename))[0]
+        output_filename = './output/' + os.path.basename(input_filename)
+        if os.path.isfile(output_filename):
+            test_func = get_compare_outputs_fn(description, input_filename, output_filename)
+            setattr(TestTexteEditsParser, 'test_{0}'.format(description), test_func)
+
+    unittest.main()
